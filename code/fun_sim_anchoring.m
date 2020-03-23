48a8f12de781a44d2db7c38174507a5fcaac3a44
lauragati <lauraember@gmail.com>
2020-03-23 16:32:38 -0400

    checked, corrected filters and bootstrap

--- code/fun_sim_anchoring.m
+++ code/fun_sim_anchoring.m
@@ -2,7 +2,7 @@
 % Generate data from anchoring model
 % A "function" copy of command_sim_anchoring.m
 % 20 March 2020
-function y = fun_sim_anchoring(param,T,N, burnin,eN,PLM,gain)
+function [y,k] = fun_sim_anchoring(param,T,N, burnin,eN,PLM,gain)
 sig_r = param.sig_r;
 sig_i = param.sig_i;
 sig_u = param.sig_u;
@@ -18,11 +18,12 @@ eta = SIG; %just so you know
 %% Simulate model
 
 y = zeros(ny,T,N);
+k = zeros(T,N);
 for n=1:N
     % Sequence of innovations
     e = squeeze(eN(:,:,n));
     
     % Learning
-    [~, y_LH] = sim_learnLH(gx,hx,SIG,T+burnin,burnin,e, Aa, Ab, As, param, PLM, gain);
+    [~, y_LH, ~, ~, ~, ~, ~, ~, ~,~, k(:,n)] = sim_learnLH(gx,hx,SIG,T+burnin,burnin,e, Aa, Ab, As, param, PLM, gain);
     y(:,:,n) = y_LH;
 end
