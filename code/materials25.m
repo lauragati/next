90340f657a7c2112e0f11f261e255d84202a3607
lauragati <lauraember@gmail.com>
2020-04-12 19:05:44 -0400

    writing up a lot of macro lunch prezi!

--- code/materials25.m
+++ code/materials25.m
@@ -20,3 +20,33 @@ skip_old_plots    = 0;
 output_table = print_figs;
 
 skip = 1;
+
+[fs, lw, grey, silver, maroon, light_coral, light_salmon,dark_green, green, light_green, light_sky_blue, ...
+    teal, purple, saddle_brown, light_brown, fs_pres,lw_pres] = plot_configs;
+
+%%
+% UMich Inflation Expectations
+series_id = 'MICH';
+observation_start = '1978-01-01';
+observation_end   = '1990-01-01';
+[output] = getFredData(series_id, observation_start, observation_end);
+umich = output.Data(:,2);
+time_umich = output.Data(:,1);
+
+
+figure
+set(gcf,'color','w'); % sets white background color
+set(gcf, 'Position', get(0, 'Screensize')); % sets the figure fullscreen
+plot(time_umich,umich, 'k', 'linewidth',lw_pres)
+ax = gca; % current axes
+ax.FontSize = fs_pres;
+grid on
+grid minor
+datetick('x','yyyy')
+if print_figs ==1
+    figname = ['macrolunch_umich']
+    cd(figpath)
+    export_fig(figname)
+    cd(current_dir)
+    close
+end
