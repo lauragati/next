804fce8c3f32c7f959b2f75debc1248103852903
lauragati <lauraember@gmail.com>
2020-04-26 16:54:51 -0400

    legendre least squares might work

--- code/materials27.m
+++ code/materials27.m
@@ -1,5 +1,5 @@
 % materials27
-% do Judd, Numerical approximation examples on 233 Mac.
+% do Judd, Numerical approximation examples 2-3 on 233 Mac.
 
 clearvars
 close all
@@ -18,11 +18,12 @@ output_table = print_figs;
 
 skip = 1;
 
-interval = linspace(-1,1,10);
-N_x = numel(interval);
-N = 5; % degree of polynomial
-ngrid = 5; % number of points used in approximation
+
 %% Let's compute some polynomials (Judd, Numerical, p. 211 Mac)
+tau = 400;
+interval = linspace(-1,1,tau);
+N = 10; % degree of polynomial (0 to N-1)
+
 legendre = @(Pn,Pn_1,n,x) (2*n+1)/(n+1)*x*Pn - n/(n+1)*Pn_1;
 
 chebyshev = @(Tn,Tn_1,n,x) 2*x*Tn -Tn_1;
@@ -31,26 +32,26 @@ laguerre = @(Ln,Ln_1,n,x) 1/(n+1)*(2*n+1-x)*Ln -n/(n+1)*Ln_1;
 
 hermite = @(Hn,Hn_1,n,x) 2*x*Hn -2*n*Hn_1;
 
-P = zeros(N,N_x);
-T = zeros(N,N_x);
-L = zeros(N,N_x);
-H = zeros(N,N_x);
+P = zeros(N,tau);
+T = zeros(N,tau);
+L = zeros(N,tau);
+H = zeros(N,tau);
 
-for i=1:N_x
+for i=1:tau
     x=interval(i);
     for n=0:N-1
         
-        if n==0
+        if n==0 % order 0
             P(n+1,i) =1;
             T(n+1,i) =1;
             L(n+1,i) =1;
             H(n+1,i) =1;
-        elseif n==1
+        elseif n==1 % order 1
             P(n+1,i) = x;
             T(n+1,i) = x;
             L(n+1,i) = 1-x;
             H(n+1,i) = 2*x;
-        else
+        else % order 2 to N-1
             P(n+1,i) = legendre(P(n,i), P(n-1,i),n,x);
             T(n+1,i) = chebyshev(T(n,i), T(n-1,i),n,x);
             L(n+1,i) = laguerre(L(n,i), L(n-1,i),n,x);
@@ -88,120 +89,101 @@ title('Hermite')
 
 % yay they work!
 
-%% Unfortunately, least squares approx requires the computation of inner products, which are integrals. So first implement some numerical integration
-% Judd, Numerical, p 258 Mac.
-% Do  it for ff=x^(1/4)
-ff = @(x) x^(1/4);
-% ff = @(x) exp(x);
-simpson = @(a,b) (b-a)/6 * (ff(a) + 4*ff((a+b)/2) + ff(b));
-a=0;
-b=1;
-% the simple simpson rule is very crude
-simpson(a,b);
 
-% so now do a composite rule which breaks the interval into n smaller
-% subintervals of length h
-n=14; % n+1 points
-h = (b-a)/n;
-x0 = a;
-f0 = ff(x0);
-clear x
-clear f
-clear summand
-for j=1:n
-    x(j) = a+j*h;
-    % Evaluate approximand at all nodes
-    f(j) = ff(x(j));
-    % check with what it should be multiplied
-    if j==n
-        m=1;
-    elseif mod(j,2)==0
-        m=2;
-    elseif mod(j,2)==1
-        m=4;
-    end
-    summand(j) = m*f(j);
-end
-composite_simpson =h/3 *(f0+ sum(summand))
 
-% you know, I'm not exactly reproducing the table in Judd, but honestly I think he
-% makes mistakes with what is n, whether it's the number of points or the
-% number of points minus 1.
+%% Example 2: approximand = (x+1)^(1/4) and  Example 3: approximand = min(max(-1,4*(x-0.2),1))
+close all
+N=4;
+ngrid =5; % number of points used in approximation
+T =100; % "size of dataset"
+interval = linspace(-1,1,T);
+a  = interval(1);
+b  = interval(end);
 
-return
-clear f
-clear x
+% Approximand in "dataset"
+% true_fun = @(x) (x+1).^(1/4);
+% To do Example 3 uncomment the following
+true_fun = @(x) min(max(-1*ones(size(x)),4.*(x-0.2)),1*ones(size(x)));
+f = true_fun(interval);
 
-%% Example 1: approximand = e^(2x+2)
-
-true1 = @(x) exp(2*x + 2);
-f1 = true1(interval);
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 1.) Legendre least squares
 % thank god the Legendre weights are just 1
-om = @(x) 1;
+om_l = @(x) 1;
 
-% For each polynomial order k
-for k=1:N
-    % each inner product is an integral
-    % use Gauss-Chebyshev quadrature (see tryouts.m)
-    a  = interval(1);
-    b  = interval(end);
-    n  = N;
-    % Then for n nodes
-    for j=1:n
-        % Calculate node
-        x(j) = a + (b-1)*(cos((2*j-1)/(2*n)*pi)+1)/2;
-        % Evaluate Legendre of order k at that node x(j) recursively
-        varphi=zeros(k,1);
-        for l=1:k
-            if l==1
-                varphi(l) =1;
-            elseif l==2
-                varphi(l) = x(j);
-            else
-                varphi(l) = legendre(varphi(l-1), varphi(l-2),1,x(j));
-            end
+x = zeros(ngrid,1);
+omq = zeros(ngrid,1);
+varphi=zeros(N+1,ngrid); % order N plus 1 because of order 0
+truth = zeros(ngrid,1);
+element_num = zeros(ngrid,1);
+element_den = zeros(ngrid,1);
+bet_ols =zeros(N+1,1);
+% Gauss-Chebyshev quadrature to evaluate the inner products
+% First loop: evaluate all the quadrature nodes, weights, Legendre
+% polynomials of all orders up to N, and approximand
+% For ngrid nodes
+for j=1:ngrid
+    % Calculate node
+    x(j) = a + (b-a)*(cos((2*j-1)/(2*ngrid)*pi)+1)/2;
+    % Calculate quadrature weights
+    omq(j) = (b-a)/2 * pi/ngrid * sqrt(1-cos((2*j-1)/(2*ngrid)*pi)^2);
+    
+    % Evaluate Legendre of order k at that node x(j) recursively
+    for k=1:N+1
+        if k==1 % order zero
+            varphi(k,j) = 1;
+        elseif k==2 % order 1
+            varphi(k,j) = x(j);
+        else % order 2 to N
+            varphi(k,j) = legendre(varphi(k-1,j), varphi(k-2,j),1,x(j));
         end
-        % Calculate quadrature weights
-        omega(j) = (b-a)/2 * pi/n * sqrt(1-cos((2*j-1)/(2*n)*pi)^2);
-        
-        % Calculate each element of the inner products
-        element_num(j) = omega(j)*true1(x(j))*varphi(end)*om(x(j));
-        element_den(j) = omega(j)*varphi(end)*varphi(end)*om(x(j));
     end
-    inner_product_num = sum(element_num);
-    inner_product_den = sum(element_den);
-    OLS = inner_product_num/inner_product_den * varphi;
+    
+    % approximand at xj
+    truth(j) = true_fun(x(j));
 end
-px = sum(OLS);
 
+% Second loop: evaluate inner products for each order k to create beta_ols
+for k=1:N+1
+    for j=1:ngrid
+        % Calculate each element of the inner products for order k
+        element_num(j) = omq(j) * truth(j)*varphi(k,j)*om_l(x(j));
+        element_den(j) = omq(j) * varphi(k,j)*varphi(k,j)*om_l(x(j));
+    end
+    inner_prod_k_num = sum(element_num);
+    inner_prod_k_den = sum(element_den);
+    bet_ols(k) = inner_prod_k_num/inner_prod_k_den;
+end
 
+% Third loop: evaluate the Legendre least squares as the fitted value over
+% the "dataset". First create Legendre polynomials over the dataset
+varphi = zeros(k,T);
+for i=1:T
+    x_i = interval(i);
+    % Evaluate Legendre of order k at that node x(j) recursively
+    for k=1:N+1
+        if k==1 % order zero
+            varphi(k,i) = 1; 
+        elseif k==2 % order 1
+            varphi(k,i) = x_i;
+        else % order 2 to N
+            varphi(k,i) = legendre(varphi(k-1,i), varphi(k-2,i),1,x_i);
+        end
+    end
+end
+px = bet_ols'*varphi;
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 2.) Chebyshev interpolation
 
-
-return
 figure
-plot(interval, f1)
+h1 = plot(interval, f); hold on
+h2 = plot(interval, px, '--');
+h3 = plot(interval, zeros(size(interval)), ':');
+h4 = plot(interval, zeros(size(interval)));
 title('True f against approximations')
-
-
-%% Example 2: approximand = (x+1)^(1/4)
-
-true2 = @(x) (x+1).^(1/4);
-f2 = true2(interval);
-
-figure
-plot(interval,f2)
-title('True f against approximations')
+legend([h1,h2, h3, h4], 'Approximand', 'Legendre least squares', 'Chebyshev interpolation', 'Spline')
 
 
 
-%% Example 3: approximand = min(max(-1,4*(x-0.2),1))
 
-true3 = @(x) min(max(-1*ones(size(x)),4.*(x-0.2)),1*ones(size(x)));
-f3 = true3(interval);
-
-figure
-plot(interval,f3)
-title('True f against approximations')
