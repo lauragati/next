27a378d56e228874b47bb99be929944ed1a142e7
lauragati <lauraember@gmail.com>
2020-09-16 17:16:17 -0400

    ran the nsearch =10 and N=1000 search
    (Saved as estim LOMgain outputs univariate coax15 Sep 2020 16 14 00.mat)

--- code/command_anchoring_in_data.m
+++ code/command_anchoring_in_data.m
@@ -13,7 +13,7 @@ todays_date = strrep(datestr(today), '-','_');
 nowstr = strrep(strrep(strrep(datestr(now), '-','_'), ' ', '_'), ':', '_');
 
 % Variable stuff ---
-print_figs        = 1;
+print_figs        = 0;
 stop_before_plots = 0;
 skip_old_plots    = 0;
 output_table = print_figs;
