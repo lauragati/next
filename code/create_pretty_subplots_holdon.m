0fe3d3424d2ae9952c1a0d32e0222c502cf9ee45
lauragati <lauraember@gmail.com>
2020-10-15 12:04:54 -0400

    work on draft

--- code/create_pretty_subplots_holdon.m
+++ code/create_pretty_subplots_holdon.m
@@ -14,22 +14,23 @@ end
 [fs, lw] = plot_configs;
 
 
-figure
+fig = figure;
 set(gcf,'color','w'); % sets white background color
 set(gcf, 'Position', get(0, 'Screensize')); % sets the figure fullscreen
 for i=1:ny
     subplot(1,ny,i)
-    h1(i) =   plot(y1(i,:), 'linewidth', lw); hold on
+    plot(zeros(1,T), 'k', 'linewidth',lw); hold on
+    h1(i) =   plot(y1(i,:), 'linewidth', lw);  
     h2(i) =   plot(y2(i,:), 'linewidth', lw, 'linestyle', '--');
-    plot(zeros(1,T), 'k--', 'linewidth',lw)
+    
     ax = gca; % current axes
     ax.FontSize = fs/1.2;
     set(gca,'TickLabelInterpreter', 'latex');
     grid on
     grid minor
     title(seriesnames{i}, 'interpreter', 'latex', 'fontsize', fs)
-    legend([h1(i), h2(i)], legendentries, 'location', 'southoutside', 'interpreter', 'latex')
-    legend('boxoff')
+%     legend([h1(i), h2(i)], legendentries, 'location', 'southoutside', 'interpreter', 'latex')
+%     legend('boxoff')
     if nargin == max_no_inputs
     xl = xlabel(xlab,'interpreter', 'latex', 'fontsize', fs/2);
     xl.Position(1) = xplus + xl.Position(1);
@@ -37,6 +38,17 @@ for i=1:ny
 %     ax.YAxis.Exponent = 0;
 %     ax.YRuler.Exponent = 0; % turns off scientific notation
 end
+
+% Overall legend
+% add a bit space to the figure
+% fig = gcf;
+% fig.Position(2) = fig.Position(2)+250;
+fig.InnerPosition = 0.9*fig.OuterPosition;
+% add legend
+Lgnd = legend('show',[h1(i), h2(i)], legendentries, 'location', 'southoutside', 'interpreter', 'latex', 'NumColumns', 2);
+legend('boxoff')
+Lgnd.Position(1) = 0.43;
+Lgnd.Position(2) = -0;
 
 
 if print_figs ==1
